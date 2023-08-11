743a8e8493fbf03510296cea73ec46b1351c86a8
Ppich00 <pichayuth90@gmail.com>
2023-08-11 21:52:34 +0700

    update line login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {liff} from "@line/liff";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,41 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'line-login';
+
+  idToken = '';
+  displayName = '';
+  pictureUrl = '';
+  statusMessage = '';
+  userId = '';
+
+  ngOnInit(): void {
+    this.initLine();
+  }
+
+  initLine(): void {
+    liff.init({liffId: '2000383540-BegEJ63g'}, () => {
+      if (liff.isLoggedIn()) {
+        this.runApp();
+      } else {
+        liff.login();
+      }
+    }, err => console.error(err));
+  }
+
+  runApp(): void {
+    const idToken = liff.getIDToken() || '';
+    this.idToken = idToken;
+    liff.getProfile().then(profile => {
+      console.log(profile);
+      this.displayName = profile.displayName;
+      this.pictureUrl = profile.pictureUrl || '';
+      this.statusMessage = profile.statusMessage || '';
+      this.userId = profile.userId;
+    }).catch(err => console.error(err));
+  }
+
+  logout(): void {
+    liff.logout();
+    window.location.reload();
+  }
 }
